3fb7d3a5abebf5c1329362bc282a832ef7dd05ac
sieujs <gamefun206@gmail.com>
2025-03-23 12:27:10 +0700

    feedbacks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommonModule } from './modules/common';
+import { CommonModule, Config } from './modules/common';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { SourceRankModule } from './modules/source-rank';
+import { BullModule } from '@nestjs/bullmq';
+import { Service } from './modules/tokens';
+import { FeedbacksModule } from './modules/feedbacks/feedbacks.module';
 
 @Module({
-  imports: [CommonModule, UserModule, AuthModule, SourceRankModule],
+  imports: [CommonModule, UserModule, AuthModule, SourceRankModule,
+    BullModule.forRootAsync( {
+      imports : [CommonModule],
+      inject: [Service.CONFIG],
+      useFactory : async (config : Config) => ({
+          connection : {
+              host : config.REDIS_HOST,
+              port : config.REDIS_PORT
+          }
+      })
+  }),
+  FeedbacksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
