392bc84a65038dabe799a8bcc2709d27a9dcc456
sieujs <gamefun206@gmail.com>
2025-05-07 16:00:38 +0700

    add 6h time

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,37 +1,41 @@
-import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
-import { UserModule } from "../user/user.module";
-import { PassportModule } from "@nestjs/passport";
-import { AuthService } from "./services/auth.service";
-import { LocalStrategy } from "./strategies/local.strategy";
-import { LocalAuthGuard } from "./guards/local.guard";
-import { JwtModule } from "@nestjs/jwt";
-import { AuthController } from "./controllers/auth.controller";
-import { JwtAdminStrategy } from "./strategies/jwt-admin.strategy";
-import { JwtUserStrategy } from "./strategies/jwt-user.strategy";
-import { NotifyModule } from "../notify/notify.module";
-import { EmailVerifyModule } from "../email-verify/email-verify.module";
-import { GoogleStrategy } from "./strategies/google.strategy";
-import { CommonModule } from "../common";
-import { OAuth2Client } from "google-auth-library";
+import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { LocalAuthGuard } from './guards/local.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './controllers/auth.controller';
+import { JwtAdminStrategy } from './strategies/jwt-admin.strategy';
+import { JwtUserStrategy } from './strategies/jwt-user.strategy';
+import { NotifyModule } from '../notify/notify.module';
+import { EmailVerifyModule } from '../email-verify/email-verify.module';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { CommonModule } from '../common';
+import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [
-        CommonModule,
-        UserModule,
-        PassportModule,
-        JwtModule.register({
-            secret: process.env.JWT_SECRET,
-            signOptions: {
-                expiresIn: '3600s',
-            },
-        }),
-        NotifyModule,
-        EmailVerifyModule,
-    ],
-    controllers : [AuthController],
-    providers: [AuthService, LocalStrategy, LocalAuthGuard, JwtAdminStrategy,JwtUserStrategy, GoogleStrategy ],
-    exports : [AuthService]
+  imports: [
+    CommonModule,
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '6h',
+      },
+    }),
+    NotifyModule,
+    EmailVerifyModule,
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    LocalAuthGuard,
+    JwtAdminStrategy,
+    JwtUserStrategy,
+    GoogleStrategy,
+  ],
+  exports: [AuthService],
 })
-export class AuthModule {
-
-}
+export class AuthModule {}
